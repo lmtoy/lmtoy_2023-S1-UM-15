f785c899b8b69aeb09aa007332ab21abfecbc5e9
Peter Teuben <teuben@gmail.com>
2024-04-02 16:07:30 -0400

    add

--- mk_runs.py
+++ mk_runs.py
@@ -10,7 +10,11 @@ project="2023-S1-UM-15"
 
 on = {}
 
-on["HB114_13CO"] =  [ 105907 ]
+on["HB114_13CO"] =  [ 105907, -105908 ]
+
+
+on["HB46"] =        [ 112931, 113062, 113063, 113065,]
+
 
 pars1 = {}
 
@@ -18,8 +22,13 @@ pars1 = {}
 #  add vlsr=0 if you want to see the local CO
 pars1["HB114_13CO"] = "dv=10 dw=30 numbands=1 restart=1 pix_list=-13 b_order=1 extent=360"
 
+pars1["HB46"] = ""
+
+
 pars2 = {}
 pars2["HB114_13CO"] = ""
+pars2["HB46"] = ""
+
 
 if __name__ == "__main__":
     runs.mk_runs(project, on, pars1, pars2, sys.argv)
