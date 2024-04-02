4e5a9934546860e5e56779cdea9435952d3fe922
Peter Teuben <teuben@gmail.com>
2024-04-02 16:55:33 -0400

    new pars

--- mk_runs.py
+++ mk_runs.py
@@ -22,7 +22,7 @@ pars1 = {}
 #  add vlsr=0 if you want to see the local CO
 pars1["HB114_13CO"] = "dv=10 dw=30 numbands=1 restart=1 pix_list=-13 b_order=1 extent=360"
 
-pars1["HB46"] = ""
+pars1["HB46"]       = "dv=10 dw=30 numbands=1 restart=1 pix_list=-13 b_order=1 extent=360"
 
 
 pars2 = {}
