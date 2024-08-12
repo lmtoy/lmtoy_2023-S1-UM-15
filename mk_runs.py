e22c685280e6cb76da5a25fca03c014baaa9363a
Peter Teuben <teuben@gmail.com>
2024-08-12 02:22:48 +0000

    upd

--- mk_runs.py
+++ mk_runs.py
@@ -10,7 +10,7 @@ project="2023-S1-UM-15"
 
 on = {}
 
-on["HB114_13CO"] =  [ 105907, -105908 ]
+#on["HB114_13CO"] =  [ 105907, -105908 ]
 
 
 on["HB46"] =        [ 112931, 113062, 113063, 113065,]
@@ -20,17 +20,18 @@ pars1 = {}
 
 #  for 200MHz bandwidth (~500 km/s) need smaller dv,dw, also to avoid birdies when you can
 #  add vlsr=0 if you want to see the local CO
-pars1["HB114_13CO"] = "dv=10 dw=30 numbands=1 restart=1 pix_list=-13 b_order=1 extent=360"
+#pars1["HB114_13CO"] = "dv=10 dw=30 numbands=1 restart=1 pix_list=-13 b_order=1 extent=360"
 
-pars1["HB46"]       = "dv=10 dw=30 numbands=1 restart=1 pix_list=-13 b_order=1 extent=360"
+pars1["HB46"]       = "dv=10 dw=30 restart=1 pix_list=-13,14,15  b_order=1 extent=360"
 
 
 pars2 = {}
-pars2["HB114_13CO"] = ""
-pars2["HB46"] = ""
-
+#pars2["HB114_13CO"] = ""
+pars2["HB46"] = "bank=0 pix_list=-13"
+pars3={}
+pars3["HB46"] = "bank=1 pix_list=-4,13,14,15"
 
 if __name__ == "__main__":
-    runs.mk_runs(project, on, pars1, pars2, sys.argv)
+    runs.mk_runs(project, on, pars1, pars2, pars3, sys.argv)
 
 
